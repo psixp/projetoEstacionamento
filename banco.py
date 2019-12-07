e417a1cdc670257a4bcf53b66aad2cd212d53a7d
psixp <pablo_six@live.com>
2019-12-06 23:11:01 -0300

    Atualização
    - adicionado:
        - Alteração no banco de dados de formato oo para procedural

--- banco.py
+++ banco.py
@@ -1,52 +1,33 @@
 import sqlite3
 
-class Banco:
-    def __init__(self):
-        self.conexao = self.createBanco()
-        #self.conexao = self.createTable()
 
-    def createBanco(self):
-        conn = sqlite3.connect('Banco_estacionamento.db').close()
-        conn.close()
-        return conn
 
-    def createTable(self):
-        c = self.conexao.cursor()
-        # tabela de placas e controle (uma para dois )
-        c.execute("""create table if not exists uso_diaria(
-                    id text autoincrement,
-                    placa text message_text primary key,
-                    d_entrada text,
-                    h_entrada text,
-                    d_saida text,
-                    h_saida text,
-                    v_pago text,
-                    bx_utilizado text,
-                    nv_uti text)""")
+conexao = sqlite3.connect('bancoEstacionamento.db')
+c = conexao.cursor()
 
-        c.execute("""create table if not exists uso_diaria(
-                    id text autoincrement,
-                    placa text,
-                    d_entrada text,
-                    h_entrada text,
-                    d_saida text,
-                    h_saida text,
-                    v_pago text,
-                    bx_utilizado text primary key)""")
+c.execute("""
+CREATE TABLE if NOT EXISTS placas(
+            placa TEXT primary key,
+            d_entrada TEXT,
+            h_entrada TEXT,
+            d_saida TEXT,
+            h_saida TEXT,
+            v_pago TEXT,
+            bx_utilizado TEXT,
+            nv_uti TEXT);
+            """)
 
-# tabela de fidelidade de clinetes
-        #c.execute("""create table if not exists fidel(
-        #             n_card_fidel message_text primary key,
-        #             placa text,
-        #             nome_user text,
-        #             cpf text,
-        #             cod_fidel int)""")
+c.execute("""
+CREATE TABLE if NOT EXISTS uso_diaria(
+           id TEXT,
+           placa TEXT,
+           d_entrada TEXT,
+           h_entrada TEXT,
+           d_saida TEXT,
+           h_saida TEXT,
+           v_pago TEXT,
+           bx_utilizado text primary key);
+           """)
 
-# tabela de nivel de fidelidade
-        #c.execute("""create table if not exists nivel_fidel(
-        #             tipo message_text primary key,
-        #             perc_desc text,
-        #             cod_fidel int)""")
-
-        self.conexao.commit()
-        c.close()
+conexao.commit()
+c.close()
